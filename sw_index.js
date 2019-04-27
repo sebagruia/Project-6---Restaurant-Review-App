5859c71b1df7e766d2eed584bad864cf61599ea5
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2019-04-27 20:45:55 +0200

    Changing the Fetch Cache event
    I will fetch the entire site instead of fetaching just some components.

--- sw_index.js
+++ sw_index.js
@@ -1,40 +1,40 @@
 const cacheName = 'v1';
-const cacheComponents = [
-    '/index.html',
-    '/restaurant.html',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/data/restaurants.json',
-    '/css/media-query.css',
-    '/css/media-query-reviews.css',
-    '/css/styles.css'
- ];
+//const cacheComponents = [
+//    '/index.html',
+//    '/restaurant.html',
+//    '/js/main.js',
+//    '/js/restaurant_info.js',
+//    '/js/dbhelper.js',
+//    '/img/1.jpg',
+//    '/img/2.jpg',
+//    '/img/3.jpg',
+//    '/img/4.jpg',
+//    '/img/5.jpg',
+//    '/img/6.jpg',
+//    '/img/7.jpg',
+//    '/img/8.jpg',
+//    '/img/9.jpg',
+//    '/img/10.jpg',
+//    '/data/restaurants.json',
+//    '/css/media-query.css',
+//    '/css/media-query-reviews.css',
+//    '/css/styles.css'
+// ];
 
 //Call Install Event
 
 self.addEventListener('install', event => {
     console.log('Service Worker: Installed');
-    event.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-            console.log('Service Worker: Caching Files');
-            cache.addAll(cacheComponents);
-        })
-
-        .then(() => self.skipWaiting())
-    );
+    //    event.waitUntil(
+    //        caches
+    //        .open(cacheName)
+    //        .then(cache => {
+    //            console.log('Service Worker: Caching Files');
+    //            cache.addAll(cacheComponents);
+    //        })
+    //
+    //        .then(() => self.skipWaiting())
+    //    );
 });
 
 //Call Activate Event
@@ -64,12 +64,22 @@ self.addEventListener('activate', event => {
 });
 
 
-//Call Fetch Event
+//Call Fetch Event (the entire site)
 
-self.addEventListener('fetch', event=>{
+self.addEventListener('fetch', event => {
     console.log('Service Worker: Fetching from Cache');
     event.respondWith(
-        fetch(event.request).catch(()=>caches.match(event.request)));
-  
-});
+        fetch(event.request)
+        .then(response => {
+            //Clone the response
+            const responseReplica = response.clone();
+            //Open a Cache
+            caches.open(cacheName).then(cache => {
+                //Adding the response to cache
+                cache.put(event.request, responseReplica);
+            })
+            return response;
+        })
+        .catch((err) => caches.match(event.request)));
 
+});
