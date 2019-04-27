7261268697ea8fd480e48075cbfc68373b0c7e2a
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2019-04-27 21:48:45 +0200

    Changed back to Caching only some parts of site
    I saw that if I use the "Caching all site method - serviceWorker sw_index2.js), this solution will generates a huge amount of data in Data Site.

--- sw_index2.js
+++ sw_index2.js
@@ -1,43 +1,11 @@
-
-// This version Caches only the bellow components of the site
+// This version Caches the entire site
 
 const cacheName = 'v1';
-const cacheComponents = [
-    '/index.html',
-    '/restaurant.html',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/data/restaurants.json',
-    '/css/media-query.css',
-    '/css/media-query-reviews.css',
-    '/css/styles.css'
- ];
 
 //Call Install Event
 
 self.addEventListener('install', event => {
     console.log('Service Worker: Installed');
-    event.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-            console.log('Service Worker: Caching Files');
-            cache.addAll(cacheComponents);
-        })
-
-        .then(() => self.skipWaiting())
-    );
 });
 
 //Call Activate Event
@@ -54,16 +22,10 @@ self.addEventListener('activate', event => {
                         console.log('Service Worker: Deleting Old Cache');
                         return caches.delete(cache);
                     }
-
                 })
-
             );
-
-
         })
-
     );
-
 });
 
 
@@ -73,6 +35,15 @@ self.addEventListener('fetch', event => {
     console.log('Service Worker: Fetching from Cache');
     event.respondWith(
         fetch(event.request)
-        .catch((err) => caches.match(event.request)));
-
+        .then(response => {
+            //Clone the response
+            const responseReplica = response.clone();
+            //Open a Cache
+            caches.open(cacheName).then(cache => {
+                //Adding the response to cache
+                cache.put(event.request, responseReplica);
+            })
+            return response;
+        })
+        .catch((err) => caches.match(event.request).then(response=>response)));
 });
